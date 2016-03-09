95e55a94281f24f850872cbdf598ad3d69d77105
Mauricio Leyzaola <mauleyzaola@users.noreply.github.com>
2016-03-09 15:12:12 -0600

    Update node.install.sh

--- node/node.install.sh
+++ node/node.install.sh
@@ -4,7 +4,7 @@ wget https://github.com/taaem/nodejs-linux-installer/releases/download/v0.3/node
 chmod +x node-install.sh && ./node-install.sh
 
 #point npm installations to local path so there is no need for sudo
-mkdir ~/npm
+mkdir -p ~/npm
 npm config set prefix ~/npm
 echo export PATH="$PATH:$HOME/npm/bin" >> ~/.bashrc
 echo export NODE_PATH="$NODE_PATH:~/npm/lib/node_modules" >>  ~/.bashrc
