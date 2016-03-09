e272c51216d8a0d58e439178a7297f81187472f2
mauleyzaola <mauricio.leyzaola@gmail.com>
2016-03-09 14:18:33 -0600

    added node.js installer

---
+++ node/node.install.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+cd ~
+wget https://github.com/taaem/nodejs-linux-installer/releases/download/v0.3/node-install.sh
+chmod +x node-install.sh && ./node-install.sh
+
+#point npm installations to local path so there is no need for sudo
+mkdir ~/npm
+npm config set prefix ~/npm
+echo export PATH="$PATH:$HOME/npm/bin" >> ~/.bashrc
+echo export NODE_PATH="$NODE_PATH:~/npm/lib/node_modules" >>  ~/.bashrc
+source ~/.bashrc
+
+# front-end dependencies recommended to install
+npm install --g bower grunt-cli
+
+rm node-install.sh
