39ffc8c0010c049460866cb96279195a8e906934
mauleyzaola <mauricio.leyzaola@gmail.com>
2016-04-04 23:19:09 -0500

    qf

--- git/git.install.sh
+++ git/git.install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get install git
+sudo apt-get install git -y
 echo "Configuring git to use ssh instead of http"
 git config --global url."git@github.com:".insteadOf "https://github.com/"
