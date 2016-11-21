dc10fc5a93a7baeda41cca496302589dda23131a
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2016-11-21 00:55:37 -0600

    Update go.install.sh

--- go/go.install.sh
+++ go/go.install.sh
@@ -2,7 +2,7 @@
 cd ~
 wget https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.7.3.linux-amd64.tar.gz
-rm go1.7.1.linux-amd64.tar.gz
+rm go1.7.3.linux-amd64.tar.gz
 
 echo '#go configuration' >> ~/.bashrc
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
