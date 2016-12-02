2167b04469e04e305def6c8a0dd14e5d7ef2335c
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2016-12-02 16:41:30 -0600

    updated to latest version

--- go/go.install.sh
+++ go/go.install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ~
-wget https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.7.3.linux-amd64.tar.gz
-rm go1.7.3.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.7.4.linux-amd64.tar.gz
+rm go1.7.4.linux-amd64.tar.gz
 
 echo '#go configuration' >> ~/.bashrc
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
