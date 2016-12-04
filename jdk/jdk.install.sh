387be0f29f9fdcdea8e08b55c0486921f00634e5
mauleyzaola <mauricio.leyzaola@gmail.com>
2016-12-04 16:17:44 -0600

    added yes confirmation

--- jdk/jdk.install.sh
+++ jdk/jdk.install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install python-software-properties
+sudo apt-get install python-software-properties -y
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update && sudo apt-get install oracle-java8-installer -y
