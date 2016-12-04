9cb54ea4f14bc0beaca83227e431975d10b1bd97
mauleyzaola <mauricio.leyzaola@gmail.com>
2016-12-04 16:21:31 -0600

    removed jdk and added cassandra db installer

--- jdk/README.md
+++
@@ -1,4 +0,0 @@
-####To install latest Oracle JDK
-```
-wget -O - https://raw.githubusercontent.com/mauleyzaola/scripts/master/jdk/jdk.install.sh | sh
-```
--- jdk/jdk.install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install python-software-properties -y
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update && sudo apt-get install oracle-java8-installer -y
