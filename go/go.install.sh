9dda2727f6c5b70b642f5ec4f71ca6705a24d7be
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2017-08-26 13:07:24 -0500

    upgrade to go 1.9

--- go/go.install.sh
+++ go/go.install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ~
-wget https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.8.3.linux-amd64.tar.gz
-rm go1.8.3.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.9.linux-amd64.tar.gz
+rm go1.9.linux-amd64.tar.gz
 
 echo '#go configuration' >> ~/.bashrc
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
