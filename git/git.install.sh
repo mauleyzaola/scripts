96f1c7b04b021859cf2c12be8cca74a225e7c069
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2018-05-03 17:53:50 -0500

    added bitbucket

--- git/git.install.sh
+++ git/git.install.sh
@@ -2,3 +2,4 @@
 sudo apt-get install git -y
 echo "Configuring git to use ssh instead of http"
 git config --global url."git@github.com:".insteadOf "https://github.com/"
+git config --global url."git@bitbucket.org:".insteadOf "https://bitbucket.org/"
