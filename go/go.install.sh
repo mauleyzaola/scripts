c46617ff0d9d1def2fb6d1daf5c07c43bdb285c7
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2019-05-07 22:23:04 -0500

    Update go.install.sh

--- go/go.install.sh
+++ go/go.install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export GOFILE="go1.12.4.linux-amd64.tar.gz"
+export GOFILE="go1.12.5.linux-amd64.tar.gz"
 cd ~
 wget "https://storage.googleapis.com/golang/$GOFILE"
 sudo tar -C /usr/local -xzf "$GOFILE"
