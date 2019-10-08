90e7e35e3da70bb8e5b2f7b4b7f97300ca5bc593
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2019-10-08 17:05:12 -0500

    Update git.install.sh

--- git/git.install.sh
+++ git/git.install.sh
@@ -3,3 +3,4 @@ sudo apt-get install git -y
 echo "Configuring git to use ssh instead of http"
 git config --global url."git@github.com:".insteadOf "https://github.com/"
 git config --global url."git@bitbucket.org:".insteadOf "https://bitbucket.org/"
+git config --global url."git@gitlab.com:".insteadOf "https://gitlab.com/"
