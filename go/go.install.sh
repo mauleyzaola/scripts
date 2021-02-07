4f3831f920abdb373306c15bd905367e9350297e
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2021-02-07 14:36:19 -0600

    Update go.install.sh

--- go/go.install.sh
+++ go/go.install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export GOFILE="go1.15.7.linux-amd64.tar.gz"
+export GOFILE="go1.15.8.linux-amd64.tar.gz"
 cd ~
 wget "https://golang.org/dl/$GOFILE"
 sudo tar -C /usr/local -xzf "$GOFILE"
